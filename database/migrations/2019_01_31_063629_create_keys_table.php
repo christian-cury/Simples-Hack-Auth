8a9c7ce8f9519558e251402457209178255570cd
Mike Santos <contato.mikesantos@gmail.com>
2019-01-31 03:06:07 -0400

    Update

--- database/migrations/2019_01_31_063629_create_keys_table.php
+++ database/migrations/2019_01_31_063629_create_keys_table.php
@@ -18,6 +18,7 @@ class CreateKeysTable extends Migration
             $table->integer('user_id');
             $table->integer('game_id');
             $table->string('token');
+            $table->longText('hwid')->nullable();
             $table->string('expiration');
             $table->timestamps();
         });
